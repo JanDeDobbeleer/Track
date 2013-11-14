692cbe1b617a65ea4b7e98a1dab48aa6798505d0
Jan Joris <jan.de.dobbeleer@gmail.com>
2013-11-14 18:28:56 +0100

    initial commit

---
+++ Track/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Track.Resources;
+
+namespace Track
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
