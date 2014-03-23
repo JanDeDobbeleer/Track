7e75b8d4a33b32ad0f5922722bb1c0beaff86572
Jan Joris <jan.dedobbeleer@twipemobile.com>
2014-03-23 21:16:16 +0100

    - added IndexedPath to fix issues with path data

---
+++ Track/Common/IndexToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Track.Common
+{
+    public class IndexToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Int32)
+                return ((int)value == 1) ? new SolidColorBrush(Colors.White) : Application.Current.Resources["PhoneInactiveBrush"];
+
+            throw new InvalidOperationException("Converter can only convert to value of type int");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Track/Controls/IndexedPath.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Track.Controls
+{
+    public class IndexedPath:Path
+    {
+        public static readonly DependencyProperty SelectedIndexBindingProperty = DependencyProperty.Register("SelectedIndex", typeof(int), typeof(IndexedPath), new PropertyMetadata(OnSelectedIndexChanged));
+
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedIndexBindingProperty); }
+            set { SetValue(SelectedIndexBindingProperty, value); }
+        }
+
+        private static void OnSelectedIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((IndexedPath)d).Fill = ((int)e.NewValue == ((IndexedPath)d).Index) ? new SolidColorBrush(Colors.White) : (SolidColorBrush)Application.Current.Resources["PhoneInactiveBrush"];
+        }
+
+        private int _index;
+        public int Index
+        {
+            get { return _index; }
+            set
+            {
+                _index = value;
+                Fill = (value == SelectedIndex) ? new SolidColorBrush(Colors.White) : (SolidColorBrush)Application.Current.Resources["PhoneInactiveBrush"];
+            }
+        }
+    }
+}
